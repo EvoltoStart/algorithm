d4af3ef1bfb0f2b4edb17f916604d38888ecbb9f
EvoltoStar <2171290799@qq.com>
2025-08-30 23:20:19 +0800

    回溯2

--- xxx/xx.go
+++ xxx/xx.go
@@ -6,7 +6,12 @@ func main() {
 	fmt.Println("xxx")
 	fmt.Println("<>")
 
+	///
+
+	fmt.Println("<>")
+
 }
+
 func subsets(nums []int) [][]int {
 	ans := make([][]int, 1<<len(nums))
 	for i := range ans { // 枚举全集 U 的所有子集 i，i就是一个子集
@@ -18,6 +23,7 @@ func subsets(nums []int) [][]int {
 	}
 	return ans
 }
+
 func permute(nums []int) (ans [][]int) {
 	n := len(nums)
 	path := make([]int, n)
@@ -37,6 +43,32 @@ func permute(nums []int) (ans [][]int) {
 			}
 		}
 	}
+	dfs(0)
+	return
+}
+
+var mapping = [...]string{"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"}
+
+func letterCombinations(digits string) (ans []string) {
+	n := len(digits)
+	if n == 0 {
+		return
+	}
+
+	path := make([]byte, n) // 注意 path 长度一开始就是 n，不是空列表
+
+	var dfs func(int)
+	dfs = func(i int) {
+		if i == n {
+			ans = append(ans, string(path))
+			return
+		}
+		for _, c := range mapping[digits[i]-'0'] {
+			path[i] = byte(c) // 直接覆盖
+			dfs(i + 1)
+		}
+	}
+
 	dfs(0)
 	return
 }
