ba7d9564420f034ee7e730780c79ae0e72d5ab52
EvoltoStar <2171290799@qq.com>
2025-08-31 23:17:07 +0800

    xxx

--- xxx/xx.go
+++ xxx/xx.go
@@ -1,12 +1,13 @@
 package xxx
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 func main() {
 	fmt.Println("xxx")
 	fmt.Println("<>")
-
-	///
 
 	fmt.Println("<>")
 
@@ -72,3 +73,72 @@ func letterCombinations(digits string) (ans []string) {
 	dfs(0)
 	return
 }
+
+func combinationSum(candidates []int, target int) (ans [][]int) {
+	n := len(candidates)
+	// 完全背包
+	f := make([][]bool, n+1) //n+1 target+1 考虑边界情况
+	f[0] = make([]bool, target+1)
+	f[0][0] = true
+	for i, x := range candidates {
+		f[i+1] = make([]bool, target+1)
+		for j, b := range f[i] {
+			f[i+1][j] = b || j >= x && f[i+1][j-x] //(b || j >= x && f[i+1][j-x]),‘=’优先级最低
+		}
+	}
+
+	path := []int{}
+	var dfs func(int, int)
+	dfs = func(i, left int) {
+		if left == 0 {
+			// 找到一个合法组合
+			ans = append(ans, slices.Clone(path))
+			return
+		}
+
+		// 无法用下标在 [0, i] 中的数字组合出 left
+		if left < 0 || !f[i+1][left] {
+			return
+		}
+
+		// 不选
+		dfs(i-1, left)
+
+		// 选
+		path = append(path, candidates[i]) // 把当前数加入路径
+		dfs(i, left-candidates[i])         // 继续用 i（因为可以重复选）
+		path = path[:len(path)-1]          // 回溯，撤销选择
+
+	}
+
+	// 倒着递归，这样参数符合 f 数组的定义
+	dfs(n-1, target)
+	return ans
+}
+
+func generateParenthesis(n int) (ans []string) {
+	path := make([]byte, n*2) // 所有括号长度都是一样的 2n
+
+	// 目前填了 left 个左括号，right 个右括号
+	var dfs func(int, int)
+	dfs = func(left, right int) {
+		if right == n { // 填完 2n 个括号
+			ans = append(ans, string(path)) // 加入答案
+			return
+		}
+		if left < n { // 可以填左括号
+			path[left+right] = '(' // 直接覆盖
+			dfs(left+1, right)
+		}
+		if right < left { // 可以填右括号
+			path[left+right] = ')' // 直接覆盖
+			dfs(left, right+1)
+		}
+	}
+
+	dfs(0, 0) // 一开始没有填括号
+	return
+}
+
+//
+//
