6948626958ede8f2df251fc47e0b4c55df47ab59
EvoltoStar <2171290799@qq.com>
2025-09-01 23:02:23 +0800

    回溯4

--- Backtracking/Backtracking.go
+++ Backtracking/Backtracking.go
@@ -1,8 +1,9 @@
-package xxx
+package main
 
 import (
 	"fmt"
 	"slices"
+	"strings"
 )
 
 func main() {
@@ -140,3 +141,122 @@ func generateParenthesis(n int) (ans []string) {
 	return
 }
 
+var dirs = []struct{ x, y int }{{0, -1}, {0, 1}, {-1, 0}, {1, 0}}
+
+func exist(board [][]byte, word string) bool {
+	cnt := map[byte]int{}
+	for _, row := range board {
+		for _, c := range row {
+			cnt[c]++
+		}
+	}
+
+	// 优化一
+	w := []byte(word)
+	wordCnt := map[byte]int{}
+	for _, c := range w {
+		wordCnt[c]++
+		if wordCnt[c] > cnt[c] {
+			return false
+		}
+	}
+
+	// 优化二
+	if cnt[w[len(w)-1]] < cnt[w[0]] {
+		slices.Reverse(w)
+	}
+
+	m, n := len(board), len(board[0])
+	var dfs func(int, int, int) bool
+	dfs = func(i, j, k int) bool {
+		if board[i][j] != w[k] { // 匹配失败
+			return false
+		}
+		if k == len(w)-1 { // 匹配成功，单词没有查到最后一个继续查找
+			return true
+		}
+		board[i][j] = 0 // 标记访问过 同一个单元格内的字母不允许被重复使用
+		for _, d := range dirs {
+			x, y := i+d.x, j+d.y // 相邻格子
+			if 0 <= x && x < m && 0 <= y && y < n && dfs(x, y, k+1) {
+				return true // 搜到了！
+			}
+		}
+		board[i][j] = w[k] // 恢复现场
+		return false       // 没搜到
+	}
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if dfs(i, j, 0) {
+				return true // 搜到了！
+			}
+		}
+	}
+	return false // 没搜到
+}
+
+func isPalindrome(s string) bool {
+	n := len(s)
+	for i := range n / 2 {
+		if s[i] != s[n-1-i] {
+			return false
+		}
+	}
+	return true
+}
+
+func partition(s string) (ans [][]string) {
+	path := []string{}
+
+	// 考虑 s 怎么分割
+	var dfs func(string)
+	dfs = func(s string) {
+		n := len(s)
+		if n == 0 { // s 分割完毕
+			ans = append(ans, slices.Clone(path))
+			return
+		}
+		for i := 1; i <= n; i++ { // 枚举子串长度
+			substr := s[:i]
+			if isPalindrome(substr) {
+				path = append(path, substr) // 分割！
+				// 考虑剩余的 s[i:] 怎么分割
+				dfs(s[i:])
+				path = path[:len(path)-1] // 恢复现场
+			}
+		}
+	}
+
+	dfs(s)
+	return
+}
+
+func solveNQueens(n int) (ans [][]string) {
+	queens := make([]int, n) // 皇后放在 (r,queens[r])
+	col := make([]bool, n)
+	diag1 := make([]bool, n*2-1)
+	diag2 := make([]bool, n*2-1)
+	var dfs func(int)
+	dfs = func(r int) {
+		if r == n {
+			board := make([]string, n)
+			for i, c := range queens {
+				board[i] = strings.Repeat(".", c) + "Q" + strings.Repeat(".", n-1-c)
+			}
+			ans = append(ans, board)
+			return
+		}
+		// 在 (r,c) 放皇后
+		for c, ok := range col {
+			rc := r - c + n - 1
+			if !ok && !diag1[r+c] && !diag2[rc] { // 判断能否放皇后
+				queens[r] = c                                    // 直接覆盖，无需恢复现场
+				col[c], diag1[r+c], diag2[rc] = true, true, true // 皇后占用了 c 列和两条斜线
+				dfs(r + 1)
+				col[c], diag1[r+c], diag2[rc] = false, false, false // 恢复现场
+			}
+		}
+	}
+	dfs(0)
+	return
+}
