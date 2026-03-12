70b122e09ffcbafbd01782eafff477bcbd5714d7
MengXiaokang <2171290799@qq.com>
2026-03-12 17:35:31 +0800

    feat(Skill): 添加多个算法实现
    - 实现了颜色分类排序功能（75题）
    - 实现了下一个排列算法（31题）
    - 实现了寻找重复数的Floyd环检测算法（287题）
    - 添加slices包用于数组操作
    - 保留原有的只出现一次的数字算法（136题）

--- Skill/main.go
+++ Skill/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "slices"
+
 // 136. 只出现一次的数字
 func singleNumber(nums []int) int {
 	ans := 0
@@ -22,4 +24,55 @@ func majorityElement(nums []int) int {
 		}
 	}
 	return ans
+}
+
+// 75. 颜色分类
+func sortColors(nums []int) {
+	p0, p1 := 0, 0
+	for i, x := range nums {
+		nums[i] = 2
+		if x <= 1 {
+			nums[p0] = 1
+			p0++
+		}
+		if x == 0 {
+			nums[p1] = 0
+			p1++
+		}
+	}
+}
+
+// 31. 下一个排列
+func nextPermutation(nums []int) {
+	n := len(nums)
+	i := n - 2
+	for i >= 0 && nums[i] >= nums[i+1] {
+		i--
+	}
+	if i >= 0 {
+		j := n - 1
+		for nums[i] >= nums[j] {
+			j--
+		}
+		nums[i], nums[j] = nums[j], nums[i]
+	}
+	slices.Reverse(nums[i+1:])
+}
+
+// 287. 寻找重复数
+func findDuplicate(nums []int) int {
+	fast, slow := 0, 0
+	for {
+		slow = nums[slow]
+		fast = nums[nums[fast]]
+		if slow == fast {
+			break
+		}
+	}
+	head := 0
+	for slow != head {
+		slow = nums[slow]
+		head = nums[head]
+	}
+	return slow
 }
